b4bebc8854f01f03e00e367ab918c79529d29ad2
lizhen.95m <lizhen.95m@bytedance.com>
2019-06-28 21:19:15 +0800

    singleton support param

--- Singleton.h
+++ Singleton.h
@@ -1,6 +1,7 @@
 #pragma once 
 
 #include <memory>
+#include <mutex>
 
 template <typename T>
 class Singleton {
@@ -9,9 +10,28 @@ public:
 	~Singleton() = delete;
 	Singleton(const Singleton &) = delete;
 	Singleton(Singleton &&) = delete;
-	static T& instance() {
-		static T ins;
-		return ins;
+	Singleton& operator=(const Singleton &) = delete;
+	Singleton& operator=(Singleton&&) = delete;
+
+	template <typename... Args>
+	static T& instance(Args&&... args){
+		std::call_once(flg,_instance<Args...>,std::forward<Args>(args)...);
+		return *pT;
 	}
+
+private:
+	template <typename... Args>
+	static void _instance(Args&&... args){
+		pT = std::make_shared<T>(std::forward<Args>(args)...);
+	}
+
+	static std::shared_ptr<T> pT;
+	static std::once_flag flg;
 };
+
+template <typename T>
+std::shared_ptr<T> Singleton<T>::pT;
+
+template <typename T>
+std::once_flag Singleton<T>::flg;
 
